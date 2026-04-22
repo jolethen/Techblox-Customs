9aaa2fbb5d5d599aa1b7b030b874d009b1237956
jolethen <155651557+jolethen@users.noreply.github.com>
2026-04-22 20:58:37 +0530

    Update ingots.lua

--- ingots.lua
+++ ingots.lua
@@ -1,18 +1,19 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 
+-- Hex color for Orange-Red: #ff4500
+-- The escape sequence format is: \27(c#ff4500)Text\27(ce)
+local orange_text = "\27(c#ff4500)Used to craft special Volcanic items and very useful as a fuel\27(ce)"
+
 minetest.register_craftitem("techblox:magma_ingot", {
-    description = S("Magma Ingot\n" .. minetest.colorize("#ff4500", "Used to craft special Volcanic items and very useful as a fuel")),
+    description = S("Magma Ingot\n" .. orange_text),
     inventory_image = "techblox_magma_ingot.png",
     stack_max = 15,
     groups = {material = 1, fuel = 1},
 })
 
--- If you want it to be usable as fuel (since it's literal lava)
-minetest.register_craft_predict(function(itemstack, player, old_craft_grid, craft_inv)
-    -- This makes it burn for a long time in a furnace
-    minetest.register_craft({
-        type = "fuel",
-        recipe = "techblox:magma_ingot",
-        burntime = 100, -- Burns much longer than coal
-    })
-end)
+-- Corrected fuel registration
+minetest.register_craft({
+    type = "fuel",
+    recipe = "techblox:magma_ingot",
+    burntime = 100,
+})
