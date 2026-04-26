613b03726cf54e44c9058a9d85cdcea2cfc33127
jolethen <155651557+jolethen@users.noreply.github.com>
2026-04-26 14:10:59 +0530

    Update ingots.lua

--- ingots.lua
+++ ingots.lua
@@ -1,17 +1,13 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 
--- Hex color for Orange-Red: #ff4500
--- The escape sequence format is: \27(c#ff4500)Text\27(ce)
-local orange_text = "\27(c#ff4500)Used to craft special Volcanic items and very useful as a fuel\27(ce)"
-
 minetest.register_craftitem("techblox:magma_ingot", {
-    description = S("Magma Ingot\n" .. orange_text),
+    description = S("Magma Ingot\nUsed to craft special Volcanic items and very useful as a fuel"),
     inventory_image = "techblox_magma_ingot.png",
     stack_max = 15,
     groups = {material = 1, fuel = 1},
 })
 
--- Corrected fuel registration
+-- Fuel registration: One ingot now powers a furnace for 100 seconds
 minetest.register_craft({
     type = "fuel",
     recipe = "techblox:magma_ingot",
